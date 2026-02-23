eaf1258930e5a8a4ec7500acaa0388b1facbd18c
Bernardo <leoaaa96@gmail.com>
2026-02-23 15:28:58 -0300

    teste de commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl text-indigo-500'>Oi eu sou um h1 perfeito e eu funciono</h1>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
